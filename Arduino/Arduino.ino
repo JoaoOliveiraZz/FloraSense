3a003c1634a32a287bf47609f908189dd3a501a0
JoaoOliveiraZz <linajoaovictor@gmail.com>
2023-10-31 19:51:23 -0300

    :construction: testing the bridge and api working together

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include<ArduinoJson.h>
+
+void setup()
+{
+
+  Serial.begin(9600);
+  // Tamanho do buffer para armazenar o JSON
+ 
+  // Serial.println();
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 
+    Serial.println("{temperature: 20,humidity: 20, iluminosity: 10}");
+
+    // Serial.println("Fazendo reaquisição POST /weather");
+
+    // String postData = "data=10";
+
+    // Serial.println("POST /weather");
+    // Serial.println(postData);
+  
+
+  // delay(60000);
 }

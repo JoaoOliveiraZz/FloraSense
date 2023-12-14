978ae91a8034d64ad27c6b7a89d2ba98d1b78453
JoaoOliveiraZz <linajoaovictor@gmail.com>
2023-12-14 12:13:11 -0300

    :zap: Adding some delay

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -11,6 +11,7 @@
 DHT dht(DHTPIN, DHTTYPE);
 
 char values[100] = "{ ", helper[10];
+int b = 0, h = 0, t = 0; 
 
 void setup()
 {
@@ -33,36 +34,36 @@ void loop()
     
 
     int brightness = analogRead(LDR);
-    brightness = map(brightness, 450, 1000, 0, 100);
+    brightness = map(brightness, 118, 780, 0, 100);
     humidity = map(humidity, 0.00, 100.00, 0, 100);
 
-
-    strcat(values, "temperature :");
-    sprintf(helper, "%d", temperature);
-    strcat(values, helper);
-  
-    strcat(values, ", humidity :");
-    sprintf(helper, "%d", humidity);
-    strcat(values, helper);
+    if(humidity <= h - h*0.10 || humidity >= h + h*0.10 || temperature <= t - t*0.10 || temperature >= t + t*0.10 || brightness <= b - 0.50 * b || brightness >= b + b*0.50){
+      strcat(values, "temperature :");
+      sprintf(helper, "%d", temperature);
+      strcat(values, helper);
     
-    strcat(values, ", brightness :");
-    sprintf(helper, "%d", brightness);
-    strcat(values, helper);
-    strcat(values, "}");
+      strcat(values, ", humidity :");
+      sprintf(helper, "%d", humidity);
+      strcat(values, helper);
+      
+      strcat(values, ", brightness :");
+      sprintf(helper, "%d", brightness);
+      strcat(values, helper);
+      strcat(values, "}");
 
-    Serial.println(values);
+      Serial.println(values);
 
-    //Serial.println("{ temperature :20, humidity :20, brightness :10}");
+      
+      b = brightness;
+      h = humidity;
+      t = temperature;
+    }
 
-    // Serial.println("Fazendo reaquisição POST /weather");
 
-    // String postData = "data=10";
+    strcpy(values, "{ ");
 
-    // Serial.println("POST /weather");
-    // Serial.println(postData);
+    delay(1000);
+
   
   
-
-    delay(5000);
-    //delay(60000);
 }
